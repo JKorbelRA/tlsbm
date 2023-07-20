c15b9d9521e9800ad0d7d2675418d842738b2b63
Jakub Korbel <jkorbel@ra.rockwell.com>
2023-07-20 15:52:19 +0200

    rough optimizations

--- scripts/heap_checker.py
+++ scripts/heap_checker.py
@@ -5,100 +5,97 @@ import numpy as np
 import argparse
 
 
-class HeapStatistics:
-
-    def file_to_points(self, heap_file):
-        with open(heap_file, "r") as heap_fd:
-            reader = csv.reader(heap_fd)
-            i = 0
-            for a_row in reader:
-                if i > 0:
-                    b_or_e = a_row[0] in ["B", "E"]
-                    alloc_point = {
-                        "op": a_row[0],
-                        "ptr": a_row[1],
-                        "orig_ptr": a_row[2] if not b_or_e else "0",
-                        "size_bytes": a_row[3]  if not b_or_e else "0",
-                    }
-                    self.alloc_points.append(alloc_point)
-                i += 1
-        self.point_cnt = i-1 if i > 0 else 0
-
-    def __init__(self, heap_file: Path):
-        self.point_cnt = 0
+class HeapStatisticsTest:
+    def __init__(self, name):
+        self.state = "parsing_context"
         self.total_alloc = 0
         self.total_free = 0
         self.used = 0
         self.peak = 0
-        self.alloc_points = []
+
+        self.context_alloc = 0
+        self.context_free = 0
+        self.context_used = 0
+        self.context_peak = 0
+
         self.cur_alloc_blocks = {}
-        self.file_to_points(heap_file)
-        self.seen_begin = False
-        self.seen_end = False
+        self.name = name
 
         self.y_axis_array = []
         self.x_axis_array = []
 
     def process_malloc(self, a_point):
-        if not self.seen_begin or self.seen_end:
-            return
+
         size_bytes = int(a_point["size_bytes"], 0)
-        self.total_alloc += size_bytes
-        self.used += size_bytes
-        self.cur_alloc_blocks[f"{a_point['ptr']}"] = size_bytes
+
+        if self.security_context_active:
+            self.context_alloc += size_bytes
+            self.context_used += size_bytes
+        else:
+            self.total_alloc += size_bytes
+            self.used += size_bytes
+
+        self.cur_alloc_blocks[f"{a_point['ptr']}"] = {
+            "size_bytes": size_bytes,
+            "type": "context" if self.security_context_active else "handshake"
+        }
 
     def process_realloc(self, a_point):
-        if not self.seen_begin or self.seen_end:
-            return
         # Free first
-        freed_size = self.cur_alloc_blocks[f"{a_point['orig_ptr']}"]
+        freed_size = self.cur_alloc_blocks[f"{a_point['orig_ptr']}"]["size_bytes"]
+        was_context = self.cur_alloc_blocks[f"{a_point['orig_ptr']}"]["type"] == "context"
+
         del self.cur_alloc_blocks[f"{a_point['ptr']}"]
 
-        self.total_free += freed_size
-        self.used -= freed_size
+        if was_context:
+            self.context_free += freed_size
+            self.context_used -= freed_size
+        else:
+            self.total_free += freed_size
+            self.used -= freed_size
 
         # Malloc next
         size_bytes = int(a_point["size_bytes"], 0)
-        self.total_alloc += size_bytes
-        self.used += size_bytes
-        self.cur_alloc_blocks[f"{a_point['ptr']}"] = size_bytes
+
+        if was_context:
+            self.context_alloc += size_bytes
+            self.context_used += size_bytes
+        else:
+            self.total_alloc += size_bytes
+            self.used += size_bytes
+
+        self.cur_alloc_blocks[f"{a_point['ptr']}"] = {
+            "size_bytes": size_bytes,
+            "type": "context" if self.security_context_active else "handshake"
+        }
 
     def process_free(self, a_point):
-        if not self.seen_begin or self.seen_end:
-            return
 
-        freed_size = self.cur_alloc_blocks[f"{a_point['ptr']}"]
+        freed_size = self.cur_alloc_blocks[f"{a_point['ptr']}"]["size_bytes"]
+        was_context = self.cur_alloc_blocks[f"{a_point['ptr']}"]["type"] == "context"
+
         del self.cur_alloc_blocks[f"{a_point['ptr']}"]
 
-        self.total_free += freed_size
-        self.used -= freed_size
-        pass
+        if was_context:
+            self.context_free += freed_size
+            self.context_used -= freed_size
+        else:
+            self.total_free += freed_size
+            self.used -= freed_size
 
-    def parse_alloc_points(self):
-        i = 0
+    def process_begin_context(self):
+        self.security_context_active = True
 
-        for a_point in self.alloc_points:
-            op = a_point["op"]
-            if op == "M":
-                self.process_malloc(a_point)
-            elif op == "R":
-                self.process_realloc(a_point)
-            elif op == "F":
-                self.process_free(a_point)
-            elif op == "B":
-                self.seen_begin = True
-            elif op == "E":
-                self.seen_end = True
-            else:
-                assert False, f"unknown op {op}"
+    def process_end_context(self):
+        self.security_context_active = False
 
-            if self.used > self.peak:
-                self.peak = self.used
+    def on_processed_point(self, i):
+        used = self.used - self.context_used
+        if used > self.peak:
+            self.peak = used
 
-            if self.seen_begin and not self.seen_end:
-                self.x_axis_array.append(i)
-                self.y_axis_array.append(self.used)
-            i += 1
+        self.x_axis_array.append(i)
+        self.y_axis_array.append(self.used)
 
     def draw(self):
         # X axis parameter:
@@ -111,12 +108,91 @@ class HeapStatistics:
         plt.show()
 
     def print_statistics(self):
-        print("--- STATISTICS ---")
-        print(f"Peak: {self.peak}")
-        print(f"Total alloc: {self.total_alloc}")
-        print(f"Total free: {self.total_free}")
-        print(f"Remaining: {self.used}")
-        assert self.total_alloc == self.total_free, "The TLS library is leaking!"
+        print(f"--- {self.name} ---")
+        print(f"Handshake Peak: {self.peak}")
+        print(f"Handshake total alloc: {self.total_alloc}")
+        print(f"Handshake total free: {self.total_free}")
+        print(f"Handshake Remaining: {self.used}")
+        assert self.total_alloc == self.total_free, "The TLS library is leaking handshake!"
+        print(f"Context Peak: {self.context_peak}")
+        print(f"Context total alloc: {self.context_alloc}")
+        print(f"Context total free: {self.context_free}")
+        print(f"Context Remaining: {self.context_used}")
+        assert self.context_alloc == self.context_free, "The TLS library is leaking context!"
+
+
+class HeapStatistics:
+
+    def file_to_points(self, heap_file):
+        with open(heap_file, "r") as heap_fd:
+            reader = csv.reader(heap_fd)
+            i = 0
+            for a_row in reader:
+                if i > 0:
+                    b_or_e = a_row[0] in ["B", "E"]
+                    alloc_point = {
+                        "op": a_row[0],
+                        "ptr": a_row[1] if not b_or_e else "0x0",
+                        "orig_ptr": a_row[2] if not b_or_e else "0x0",
+                        "size_bytes": a_row[3] if not b_or_e else "0",
+                        "name": a_row[1] if b_or_e else None
+                    }
+                    self.alloc_points.append(alloc_point)
+                i += 1
+        self.point_cnt = i-1 if i > 0 else 0
+
+    def __init__(self, heap_file: Path):
+        self.point_cnt = 0
+        self.tests = []
+        self.cur_test = None
+        self.alloc_points = []
+        self.file_to_points(heap_file)
+
+    def process_begin_marker(self, a_point):
+        name = a_point["name"]
+        if name.startswith("Test:"):
+            hst = HeapStatisticsTest(name)
+            self.tests.append(hst)
+            self.cur_test = hst
+        if name == "Context":
+            self.cur_test.process_begin_context()
+
+    def process_end_marker(self, a_point):
+        name = a_point["name"]
+        if name.startswith("Test:"):
+            self.cur_test = None
+        if name == "Context":
+            self.cur_test.process_end_context()
+
+    def parse_alloc_points(self):
+        i = 0
+
+        for a_point in self.alloc_points:
+            op = a_point["op"]
+            if op == "M":
+                self.cur_test.process_malloc(a_point)
+            elif op == "R":
+                self.cur_test.process_realloc(a_point)
+            elif op == "F":
+                self.cur_test.process_free(a_point)
+            elif op == "B":
+                self.process_begin_marker(a_point)
+            elif op == "E":
+                self.process_end_marker(a_point)
+            else:
+                assert False, f"unknown op {op}"
+
+            if self.cur_test is not None:
+                self.cur_test.on_processed_point(i)
+            i += 1
+
+    def draw(self):
+        for a_test in self.tests:
+            a_test.draw()
+
+    def print_statistics(self):
+        for a_test in self.tests:
+            a_test.print_statistics()
 
 
 if __name__ == '__main__':
--- wolfssl/user_settings.h
+++ wolfssl/user_settings.h
@@ -5,5 +5,17 @@
 #define XMALLOC_USER
 #undef NO_PSK
 
+#undef ALT_ECC_SIZE
+#define SMALL_SESSION_CACHE
+#define USE_SLOW_SHA2
+#define GCM_SMALL
+#define NO_OLD_TLS
+#define RSA_LOW_MEM
+#define CURVE25519_SMALL
+#define WOLFSSL_SMALL_CERT_VERIFY
+#define HAVE_CHACHA
+#define HAVE_POLY1305
+#define NO_OLD_POLY1305
+
 
 #endif // WOLF_USER_SETTINGS_H
